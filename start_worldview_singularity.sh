a654e9be027a3baa0597e300eb33de8d2dfb6cf9
Nic Hannah <nic@doubleprecision.com.au>
2020-12-01 00:21:51 +1100

    Fix bug in buildOptions, new start and stop scripts.

--- start_worldview_singularity.sh
+++ start_worldview_singularity.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-singularity instance start worldview_latest.sif worldview
-# FIXME: create worldview.def that makes worldview_latest.sif with correct entrypoint
-singularity exec instance://worldview export PORT=9000 nohup /bin/sh ./tasks/start.sh &
+rm $HOME/.singularity/instances/logs/irsweb02/nhannah/worldview.err
+rm $HOME/.singularity/instances/logs/irsweb02/nhannah/worldview.out
+
+singularity instance start worldview.sif worldview
+# FIXME: create worldview.def that makes worldview.sif with correct entrypoint
+#singularity exec instance://worldview export PORT=3000 nohup /bin/sh ./tasks/start.sh &
